93c25041729392111dcee1cb3a5f9c1a10934bb3
Pedro Augusto <pedro.silva@vortigo.com.br>
2025-04-21 20:49:12 -0300

    feat: criar migração para roles e permissões usando Spatie

---
+++ database/migrations/2025_04_21_233833_create_roles_and_permissions.php
@@ -0,0 +1,56 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\DB;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        // Criação das permissões
+        $permissions = ['ver agenda', 'editar perfil', 'trocar senha'];
+        $vendedorPermissions = ['gerenciar vendas', 'nova venda', 'vendas em andamento', 'vendas finalizadas'];
+        $adminPermissions = ['gerenciar usuarios'];
+
+        // Criar todas as permissões
+        foreach (array_merge($permissions, $vendedorPermissions, $adminPermissions) as $permission) {
+            Permission::create(['name' => $permission]);
+        }
+
+        // Criação do perfil administrador
+        $adminRole = Role::create(['name' => 'administrador']);
+        $adminRole->givePermissionTo(Permission::all()); // Todas as permissões
+
+        // Criação do perfil vendedor
+        $vendedorRole = Role::create(['name' => 'vendedor']);
+        $vendedorRole->givePermissionTo(array_merge($permissions, $vendedorPermissions)); // Permissões específicas
+
+        // Atribuir o papel de administrador ao usuário com o e-mail especificado
+        $user = User::where('email', 'pedrocaaugusto@gmail.com')->first();
+        if ($user) {
+            $user->assignRole('administrador');
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        // Remover os perfis
+        Role::whereIn('name', ['vendedor', 'administrador'])->delete();
+
+        // Remover as permissões
+        Permission::whereIn('name', [
+            'ver agenda', 'editar perfil', 'trocar senha',
+            'gerenciar vendas', 'nova venda', 'vendas em andamento', 'vendas finalizadas',
+            'gerenciar usuarios'
+        ])->delete();
+    }
+};
